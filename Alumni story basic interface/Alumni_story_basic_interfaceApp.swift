de396e7cd1a509a5b06a10ec11650b4b947f1d7c
Hayden Chan <hchan@lcs.on.ca>
2024-11-27 11:32:55 -0500

    Initial Commit

---
+++ Alumni story basic interface/Alumni_story_basic_interfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alumni_story_basic_interfaceApp.swift
+//  Alumni story basic interface
+//
+//  Created by hayden on 2024-11-27.
+//
+
+import SwiftUI
+
+@main
+struct Alumni_story_basic_interfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alumni story basic interface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alumni story basic interface
+//
+//  Created by hayden on 2024-11-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
